31d30a44d516675b26a6f51530ba750a43fee69f
usbbush <jeanbienvenusb@gmail.com>
2023-01-18 20:38:19 +0200

    Redirect based on role after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(_resource_or_scope)
     # Add path here
-    authenticated_root_path
+    if current_user.admin?
+      admin_root_path
+    else
+      authenticated_root_path
+    end
   end
 end
