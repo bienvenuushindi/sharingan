213ba391295a1e4e9a1529e70fc5aa057f386abd
usbbush <jeanbienvenusb@gmail.com>
2023-01-18 19:00:10 +0200

    Add admin? function

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :searches
   validates :email, presence: true, uniqueness: true, email: true
   validates :password, presence: true, length: { minimum: 6 }, on: :create
+
+  protected
+
+  def admin?
+    role == 'admin'
+  end
 end
