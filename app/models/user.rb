a9fea308db0d27e0cfcca2eca6175f77b3702447
usbbush <jeanbienvenusb@gmail.com>
2023-01-18 18:31:38 +0200

    Implement a custom  email validator

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    return if value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+    record.errors.add attribute, (options[:message] || 'is not an email')
+  end
+end
+
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
