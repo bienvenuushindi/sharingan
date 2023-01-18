5aa8b70a52ad24494aa7f0baf54512d2c21dccf5
usbbush <jeanbienvenusb@gmail.com>
2023-01-18 22:38:27 +0200

    Add Initial test

---
+++ test/system/searches_test.rb
@@ -0,0 +1,49 @@
+require 'application_system_test_case'
+
+class SearchesTest < ApplicationSystemTestCase
+  setup do
+    @search = searches(:one)
+  end
+
+  test 'visiting the index' do
+    visit searches_url
+    assert_selector 'h1', text: 'Searches'
+  end
+
+  test 'should create search' do
+    visit searches_url
+    click_on 'New search'
+
+    fill_in 'Article count', with: @search.article_count
+    fill_in 'Occurence', with: @search.occurence
+    fill_in 'Term', with: @search.term
+    fill_in 'User count', with: @search.user_count
+    fill_in 'User', with: @search.user_id
+    click_on 'Create Search'
+
+    assert_text 'Search was successfully created'
+    click_on 'Back'
+  end
+
+  test 'should update Search' do
+    visit search_url(@search)
+    click_on 'Edit this search', match: :first
+
+    fill_in 'Article count', with: @search.article_count
+    fill_in 'Occurence', with: @search.occurence
+    fill_in 'Term', with: @search.term
+    fill_in 'User count', with: @search.user_count
+    fill_in 'User', with: @search.user_id
+    click_on 'Update Search'
+
+    assert_text 'Search was successfully updated'
+    click_on 'Back'
+  end
+
+  test 'should destroy Search' do
+    visit search_url(@search)
+    click_on 'Destroy this search', match: :first
+
+    assert_text 'Search was successfully destroyed'
+  end
+end
