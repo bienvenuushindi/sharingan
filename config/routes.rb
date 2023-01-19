07d64deb8babf5578f76d531217a968424a8a2ff
usbbush <jeanbienvenusb@gmail.com>
2023-01-19 15:05:09 +0200

    Add route for visited article

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   end
   resources :searches,  only: [:create, :index]
   resources :articles
+  get '/search/:article_id(/:term)', to: 'searches#add', as: 'visit'
 end
