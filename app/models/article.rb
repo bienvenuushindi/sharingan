d40a6d27ea96d684e5ff845d7ee46041969546ce
usbbush <jeanbienvenusb@gmail.com>
2023-01-19 16:04:06 +0200

    Add method to encapsulate model operation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,15 @@ class Article < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search, against: %i[title], using: { tsearch: { prefix: true } }
   belongs_to :user
+  has_and_belongs_to_many :searches
   validates :title, presence: true
   validates :body, presence: true
+
+  def update_visited_count
+    increment!(:visited_count)
+  end
+
+  def sort_by_visited
+    order('visited_count desc')
+  end
 end
