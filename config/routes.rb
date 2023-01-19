a704936193a54cd2791b1090210b98c2f42df0f5
usbbush <jeanbienvenusb@gmail.com>
2023-01-19 17:58:05 +0200

    Add route for statistics

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   devise_scope :user do
     authenticated :user do
-      constraints RoleRouteConstraint.new { |user| user.admin? } do
+      # constraints RoleRouteConstraint.new { |user| user.admin? } do
         root 'home#index', as: :admin_root
-      end
-      root 'searches#index', as: :authenticated_root
+      # end
+      # root 'searches#index', as: :authenticated_root
     end
     unauthenticated do
       root 'devise/sessions#new', as: :unauthenticated_root
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   resources :searches,  only: [:create, :index]
   resources :articles
   get '/search/:article_id(/:term)', to: 'searches#add', as: 'visit'
+  get '/keyword/:id', to: 'home#statistics', as: 'statistics'
 end
