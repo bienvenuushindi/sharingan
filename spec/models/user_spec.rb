fe029df8de2241d5b147aec6df1f03ffb7495d03
usbbush <jeanbienvenusb@gmail.com>
2023-01-19 18:55:49 +0200

    Write spec for user,search, article models

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+RSpec.describe 'User', type: :model do
+  subject do
+    User.new(email: 'jeanbienvenusb@gmail.com', password: 'my@password',
+             password_confirmation: 'my@password')
+  end
+  before { subject.save }
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'email should be present' do
+    subject.email = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'role should can be blank' do
+    subject.role = nil
+    expect(subject).to be_valid
+  end
+  it 'password should be present' do
+    subject.password = nil
+    expect(subject).to_not be_valid
+  end
+end
