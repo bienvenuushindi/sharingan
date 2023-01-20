9c7e4bce2dd8952e838434f766a6ff125193b695
usbbush <jeanbienvenusb@gmail.com>
2023-01-20 05:39:16 +0200

    Fix rubocop issues

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -38,12 +38,12 @@ class SearchesController < ApplicationController
 
     # find similar term order by popularity
     popular_articles = Search.where.not(id: user_searches.pluck(:id))
-                             .search(term).order('occurrence desc')
-                             .includes([:articles]).uniq.map(&:articles).flatten
+      .search(term).order('occurrence desc')
+      .includes([:articles]).uniq.map(&:articles).flatten
 
     # find articles by most visited
     articles = Article.where.not(id: popular_articles.pluck(:id).union(preference_articles))
-                      .search(term).order('visited_count desc').uniq
+      .search(term).order('visited_count desc').uniq
 
     # combine results
     @searches = preference_articles.concat(popular_articles).concat(articles)
