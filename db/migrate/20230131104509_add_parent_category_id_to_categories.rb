e94418ff9d0876403344e2b992417939853638c6
usbbush <jeanbienvenusb@gmail.com>
2023-01-31 13:05:55 +0200

    Correct default value

--- db/migrate/20230131104509_add_parent_category_id_to_categories.rb
+++ db/migrate/20230131104509_add_parent_category_id_to_categories.rb
@@ -1,5 +1,5 @@
 class AddParentCategoryIdToCategories < ActiveRecord::Migration[7.0]
   def change
-    add_column :categories, :parent_category_id, :integer, index: true, default: null
+    add_column :categories, :parent_category_id, :integer, index: true, default: nil
   end
 end
