caeecb9e268510f96da27f808cf821402667fa4e
usbbush <jeanbienvenusb@gmail.com>
2023-02-02 10:12:06 +0200

    Make sidebar responsive

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
+import {closeMenu} from "../custom";
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
+    connect() {
+        // this.element.textContent = "Hello World!"
+        // closeMenu()
+    }
+
+    sideMenu = document.getElementById('side-menu');
+    mainSection = document.getElementById('main');
+
+    openMenu() {
+        this.sideMenu.classList.remove('left-[-250px]');
+        this.sideMenu.classList.add('left-0');
+        this.mainSection.classList.add('xl:ml-64');
+    }
+
+    closeMenu() {
+        this.sideMenu.classList.remove('left-0');
+        this.sideMenu.classList.add('left-[-250px]');
+        this.mainSection.classList.remove('xl:ml-64');
+    }
 }
