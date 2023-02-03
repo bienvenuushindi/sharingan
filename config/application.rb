15fc8bf634435ef7ce58f7ff76793b3f1f7f83ad
usbbush <jeanbienvenusb@gmail.com>
2023-02-03 15:32:08 +0200

    Style markdown content

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative "boot"
 
 require "rails/all"
 require 'github/markup'
-require 'commonmarker'
+# require 'commonmarker'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
