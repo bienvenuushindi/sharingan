77a813ac9d9551fbda0ab5879e6a129ee734f434
usbbush <jeanbienvenusb@gmail.com>
2023-02-07 00:11:12 +0200

    Abstract some model's methods

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,6 +12,6 @@ class Category < ApplicationRecord
 
   scope :guidelines_categories, -> { where(parent_category_id: Category.select('id').where(name: 'Guidelines')) }
   scope :projects_categories, -> { where(parent_category_id: Category.select('id').where(name: 'Projects')) }
-  scope :general_req, -> { where('name LIKE ?', 'general%') }
-  scope :filter_out_gen_req, -> { where('name NOT LIKE ?', 'general%') }
+  scope :general_req, -> { starts_with('name', 'general') }
+  scope :filter_out_gen_req, -> { starts_not_with('name', 'general') }
 end
