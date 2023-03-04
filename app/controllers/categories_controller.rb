1126ccb788965258481439d6e704d033a4bab5be
usbbush <jeanbienvenusb@gmail.com>
2023-03-04 13:07:39 +0200

    Improve UI and UX

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.all.order('name')
   end
 
   # GET /categories/1 or /categories/1.json
