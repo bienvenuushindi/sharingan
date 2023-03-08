279ef788c0a15ea31871d6800937f048a40435c8
usbbush <jeanbienvenusb@gmail.com>
2023-03-08 17:03:33 +0200

    Remove edit action from filter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   load_and_authorize_resource
   before_action :set_article, only: %i[show edit update destroy]
-  before_action :set_select_collections, only: %i[edit update new create]
+  before_action :set_select_collections, only: %i[update new create]
   # GET /articles or /articles.json
   def index
     @articles = Article.all
