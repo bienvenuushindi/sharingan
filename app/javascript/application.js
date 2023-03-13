a5caf381005db08b1aeddcd17e7484d800638141
usbbush <jeanbienvenusb@gmail.com>
2023-03-13 16:04:30 +0200

    Import pager module

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,4 +5,5 @@ import "hotkeys-js"
 import "@rails/request.js"
 import "./controllers/index.js"
 import "easymde"
+import "./lib/PagerJs.js"
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
