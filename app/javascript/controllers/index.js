240c51d9ddd2b9c8f159f16c8d9f99b0d0bcd5b0
usbbush <jeanbienvenusb@gmail.com>
2023-04-07 17:11:20 +0200

    Add file extension

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,9 +6,9 @@ import {application} from "./application.js"
 import HelloController from "./hello-controller.js"
 import SearchFormController from "./search_form-controller.js"
 import AdminController from "./admin-controller.js"
-import PaginationController from "./pagination_controller";
-import ModalController from "./modal_controller";
-import ToastController from "./toast_controller";
+import PaginationController from "./pagination_controller.js";
+import ModalController from "./modal_controller.js";
+import ToastController from "./toast_controller.js";
 
 application.register("hello", HelloController)
 application.register("search-form", SearchFormController)
