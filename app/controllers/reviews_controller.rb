e4cae824d69a9d5a9c5f9fa01abc6e52635abe55
usbbush <jeanbienvenusb@gmail.com>
2023-04-07 16:53:58 +0200

    Move render turbo_stream template to views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,10 +4,7 @@ class ReviewsController < ApplicationController
   def checklist
     @list = Category.find(params[:category]).articles
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace(params[:origin], render_to_string(partial: 'reviews/checklist'),
-                                                  locals: { list: @list })
-      end
+      format.turbo_stream
 
       format.html { redirect_to new_article_url }
     end
@@ -16,11 +13,7 @@ class ReviewsController < ApplicationController
   def fetch_body
     @body = Article.select('id, body').where(id: params[:id]).first
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.prepend('review', render_to_string(partial: 'reviews/changes'),
-                                                  locals: { body: @body })
-      end
-
+      format.turbo_stream
       format.html { redirect_to new_article_url }
     end
   end
