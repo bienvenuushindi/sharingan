172b04e5659ee9566dd48f71c7372cb73c24336f
usbbush <jeanbienvenusb@gmail.com>
2023-04-07 16:51:00 +0200

    Import Jquery

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,4 +6,9 @@ import "@rails/request.js"
 import "./controllers/index.js"
 import "easymde"
 import "./lib/PagerJs.js"
+
+
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
