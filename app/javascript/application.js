00e204b1f8f4e6336868ef9956eb2f41052355d1
usbbush <jeanbienvenusb@gmail.com>
2023-04-11 15:08:36 +0200

    Add confirm with text

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -9,6 +9,34 @@ import "./lib/PagerJs.js"
 
 
 import jquery from 'jquery'
+
 window.jQuery = jquery
 window.$ = jquery
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+Turbo.setConfirmMethod((message, element) => {
+    let confirmText = element.dataset.turboConfirmText
+    let description = element.dataset.turboConfirmDescription || ""
+    let dialog = $('#turbo-confirm')
+    let confirmField = dialog.find("[data-behavior='confirm-text']")
+    let commitButton = dialog.find("button[value='confirm']")
+    dialog.find("[data-behavior='title']").text(message)
+    dialog.find("[data-behavior='description']").html(description)
+    confirmField.prop('value', '')
+    if (confirmText) {
+        confirmField.show()
+        commitButton.prop('disabled', true)
+        confirmField.get(0).addEventListener("input", (ev) => {
+            commitButton.prop('disabled', (ev.target.value !== confirmText))
+        })
+    } else {
+        confirmField.hide()
+    }
+
+    dialog.get(0).showModal()
+    return new Promise((resolve, reject) => {
+        dialog.get(0).addEventListener("close", () => {
+            resolve(dialog.get(0).returnValue === "confirm")
+        }, {once: true})
+    })
+})
