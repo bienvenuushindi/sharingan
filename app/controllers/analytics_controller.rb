a87a0e94490d1f0942b81e4b1d1efce46264ab2b
usbbush <jeanbienvenusb@gmail.com>
2023-04-17 21:18:21 +0200

    Add search service

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -3,97 +3,59 @@ class AnalyticsController < ApplicationController
   before_action :set_search, only: [:show]
   # GET /analytics or /analytics.json
   def index
+    @searches = []
     if params_exist?
-      @search_term = search_params
-      find_best_match(@search_term, categories_params?)
-      time = DateTime.now.strftime('%Q')
-      redis_key = "user:#{current_user.id}"
-      REDIS.lpush(
-        redis_key,
-        {
-          term: search_params,
-          created_at: time
-        }.to_json
-      )
-    else
-      @searches = []
+      @pagy, @searches = pagy(Search.find_best_match(search_params, categories_params?), items: 6)
+      Search.push_to_redis(REDIS, { creator: current_user, value: search_params })
     end
     if turbo_frame_request?
       if params[:commit].present?
-        create(@search_term)
+        create(search_params)
       else
-        render partial: 'searches', locals: { searches: @searches, term: @search_term }
+        render partial: 'searches', locals: { searches: @searches, term: search_params }
       end
     else
       render 'index', locals: { searches: @searches, term: '' }
     end
   end
 
+  def statistics
+    @searches = Analytic.all.order('occurrence desc').limit(30)
+    @users = User.where(role: 'user').order('email desc').limit(30)
+    @articles = Article.all.order('visited_count desc').limit(30)
+  end
+
+  def show_stats
+    @search = Analytic.find(params[:id])
+    @users = @search.users.order('email desc').limit(30).uniq
+    @articles = @search.articles.includes([:user]).order('visited_count desc').limit(30).uniq
+  end
+
   def show; end
 
   def add
-    @article = Article.find(params[:article_id])
+    article = Article.find(params[:article_id])
     if params_exist?
-      term = search_params
-      create(term, from_article: true)
-      flash[:notice] = @search.save ? 'Search was successfully created.' : 'Search insertion failed.'
-      @article.update_visited_count
+      flash[:notice] = if Search.insert(current_user, search_params, article)
+                         'Search was successfully created.'
+                       else
+                         'Search insertion failed.'
+                       end
+
     end
-    redirect_to article_path(@article)
-  end
-
-  def find_best_match(term = '', categories = [])
-    if categories.empty?
-
-      # p current_user.analytics.uniq.pluck(:id)
-      # user_searches = current_user.analytics.search(term)
-
-      # find user preferences
-      # preference_articles = user_searches.includes(articles: [:categories]).uniq.map(&:articles).flatten
-
-      # find similar term order by popularity
-      # popular_articles = Analytic.where.not(id: user_searches.pluck(:id))
-      #   .search(term).order('occurrence desc')
-      #   .includes(articles: [:categories]).uniq.map(&:articles).flatten
-
-      # find articles by most visited
-      # articles = Article.where.not(id: popular_articles.pluck(:id).union(preference_articles))
-      #                   .search(term).includes([:categories]).order('visited_count desc').uniq
-      cat_articles = if params[:category].present?
-                       Category.includes([:articles]).projects_categories.uniq.map(&:articles).flatten.pluck(:id)
-                     else
-                       Category.includes([:articles]).guidelines_categories.uniq.map(&:articles).flatten.pluck(:id)
-                     end
-      articles = Article.includes([:categories]).where(id: cat_articles).search(term).order('visited_count desc')
-
-      @pagy, @searches = pagy(articles, items: 6)
-    else
-      articles = Category.includes([:articles]).where(id: categories).uniq.map(&:articles).flatten
-      @pagy, @searches = pagy(
-        Article.includes([:categories]).where(id: articles).search(term).order('visited_count desc'), items: 6
-      )
-    end
+    redirect_to article_path(article)
   end
 
   # POST /analytics or /analytics.json
-  def create(term, from_article: false)
-    @search = Analytic.where(term: term.downcase).first_or_initialize
-    @search.add_user(current_user)
-
-    # @search.update_occurrence
-    # @search.update_user_count
-    if from_article
-      @search.update_article_count
-      @search.articles << @article
-    end
-    flash[:notice] = @search.save ? 'Search was successfully created.' : 'Search insertion failed.'
+  def create(term)
+    flash[:notice] = Search.insert(current_user, term) ? 'Search was successfully created.' : 'Search insertion failed.'
   end
 
   def select_page
     if current_user.admin?
       admin_dashboard
     else
-      visitor_page
+      user_page
     end
   end
 
@@ -104,9 +66,9 @@ class AnalyticsController < ApplicationController
     redirect_to admin_root_url
   end
 
-  def visitor_page
+  def user_page
     # load data required for operators
-    redirect_to analytics_url
+    redirect_to search_url
   end
 
   def search_params
