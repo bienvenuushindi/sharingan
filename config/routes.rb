e620b2e883bb324d64962ab974b659705c9c19fe
usbbush <jeanbienvenusb@gmail.com>
2023-04-17 21:21:09 +0200

    Update routes controllers action

--- config/routes.rb
+++ config/routes.rb
@@ -12,14 +12,15 @@ Rails.application.routes.draw do
       root 'devise/sessions#new', as: :unauthenticated_root
     end
   end
-  resources :analytics, only: [:create, :index, :show]
+  resources :analytics, only: [:create, :show]
   resources :articles
+  get '/search', to: 'analytics#index', as: 'search'
   get '/search/:article_id(/:term)', to: 'analytics#add', as: 'visit'
   post '/checklist(/:category)', to: 'reviews#checklist', as: 'checklist'
   get '/review/:id', to: 'reviews#fetch_body', as: 'get_body'
   get '/search-by(/:category)', to: 'reviews#index', as: 'search_by_category'
   post '/group-by(/:category)', to: 'categories#group_by_category', as: 'group'
   post '/general-requirement', to: 'categories#general_requirement', as: 'gen-req'
-  get '/keyword/:id', to: 'home#statistics', as: 'queries'
-  get '/dashboard', to: 'home#index', as: 'admin_root'
+  get '/keyword/:id', to: 'analytics#show_stats', as: 'queries'
+  get '/statistics', to: 'analytics#statistics', as: 'admin_root'
 end
